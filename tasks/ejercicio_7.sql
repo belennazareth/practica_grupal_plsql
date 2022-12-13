9c9f31903ef9e8260abc5fead35cd0ca6b101971
Gonzalo Marin <94838508+gonzalomaring@users.noreply.github.com>
2022-12-13 11:05:37 +0100

    Solución ejercicio 7

--- tasks/ejercicio_7.sql
+++ tasks/ejercicio_7.sql
@@ -1,3 +1,16 @@
 -- 7. Realiza los módulos de programación necesarios para evitar que un investigador figure como 
 -- colaborador de un experimento del que es responsable.
 
+create or replace trigger ejercicio7 before insert or update on Colaboraciones for each row
+declare
+l_codexp experimentos.codigo%type;
+begin
+select codigo into l_codexp from Experimentos where nifinvestigador = :new.NIFInvestigador and nifinvestigador in(select :new.nifinvestigador from colaboraciones where :new.codigoexperimento =(select codigo from experimentos where nifinvestigador = :new.nifinvestigador));
+    if l_codexp = :new.codigoexperimento then
+        raise_application_error(-20004, 'El investigador no puede ser colaborador de un experimento del que es responsable');
+    end if;
+exception
+when no_data_found then
+    null;
+end ejercicio7;
+/
