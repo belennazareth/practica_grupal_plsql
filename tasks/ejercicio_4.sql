b26ba88f2796d01868f349e77368c9377b9d80ef
Belén Nazareth Durán <belennazareth29@gmail.com>
2022-12-22 12:40:06 +0100

    ejercicio 8  ٩(╬ʘ益ʘ╬)۶

--- tasks/ejercicio_4.sql
+++ tasks/ejercicio_4.sql
@@ -2,3 +2,12 @@
 -- por correo electrónico al investigador responsable del experimento, incluyendo en el correo la 
 -- fecha de la prueba, el aspecto valorado y donde vive el catador.
 
+create trigger puntuacion_menor_5
+after insert on Puntuaciones
+for each row
+if new.valor < 5
+begin
+    declare email varchar(50);
+    select email into email from investigador where id_investigador = new.id_investigador;
+    call enviar_correo(email, 'Puntuación menor de 5', concat('La puntuación de la prueba ', new.id_prueba, ' es menor de 5'));
+end
